ecb79d1081a9f6fbac2cca7c023e99724d16e027
MCarroll138 <Mcarroll138@gmail.com>
2023-06-09 10:14:14 -0700

    JS Business Logic, tests of input rules for text input equal to integers.

---
+++ js/scripts.js
@@ -0,0 +1,91 @@
+//Business Logic
+
+let userNumber = []
+
+function getArray(text) {
+  let userInput = text.forEach;
+  console.log(userInput)
+}
+
+/*function countUp(countTo, countBy) {
+     if (badInputs.includes(parseInt(countTo)) || badInputs.includes(parseInt(countBy))) {
+  return "Enter a number!";
+ } else if (Math.sign(countTo) === -1 || Math.sign(countBy) === -1) {
+  return "Enter a positive number!";
+ }
+  for (let i = countBy; i <= countTo; i +=countBy) {
+      finalResult.push(i);
+  }
+  return finalResult;
+  
+};
+
+/*function countUp(countTo, countBy) {
+  let finalResult = [];
+  const badInputs = ["", NaN];
+ if (badInputs.includes(parseInt(countTo)) || badInputs.includes(parseInt(countBy))) {
+  return "Enter a number!";
+ } else if (Math.sign(countTo) === -1 || Math.sign(countBy) === -1) {
+  return "Enter a positive number!";
+ }
+  for (let i = countBy; i <= countTo; i +=countBy) {
+      finalResult.push(i);
+  }
+  return finalResult;
+  
+};
+
+\*
+
+//Business logic
+
+// function pigLatin(vowel, text) {
+//     const passage = text;
+//     let passageLength = passage.length;
+
+//     // const vowelArray = ['a' ,'e' ,'i' ,'o' ,'u']
+//     for (let i = 0; i <= passageLength; i++ )
+//     if passag
+
+// }
+function startsWithVowel(text){
+  let word = text;
+  if (word.startsWith('a') || word.startsWith('e') ||word.startsWith('i') ||word.startsWith('o') ||word.startsWith('u')){
+      return true;
+  }
+  return false;
+}
+
+
+
+function pigLatin(text){
+ newWord = []
+  let passage = text.split(" ");
+passage.foreach(function(word){
+  if (startsWithVowel(word)){
+      newWord.push(word + "way");
+  }
+  else if(!startsWithVowel(passage)){
+      newWord.push(word + "const");
+  }
+  
+  // return word;
+})
+return newWord;
+}
+*/
+const numbers = [10, 23, 34, 44];
+numbers.forEach(function (number) {
+  alert('I Love ' + number);
+});
+
+function containsInt(text) {
+  let intSplit = text.toString().split(" ");
+  let integers = intSplit.map(Number);
+  console.log(integers)
+
+}
+
+
+
+//User Logic
